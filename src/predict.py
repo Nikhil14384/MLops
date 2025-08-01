044e8a3a699251af71fc93f3ef2cc69f361160b3
Nareddula Nikhil Kumar Reddy <36074690+Nikhil14384@users.noreply.github.com>
2025-08-01 10:04:22 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,10 @@
-# Prediction logic
+import pandas as pd
+import joblib
+
+def load_model(path="models/best_model.pkl"):
+    return joblib.load(path)
+
+def predict(model, input_data: list):
+    df = pd.DataFrame([input_data], columns=["sepal length (cm)", "sepal width (cm)", "petal length (cm)", "petal width (cm)"])
+    prediction = model.predict(df)
+    return int(prediction[0])
